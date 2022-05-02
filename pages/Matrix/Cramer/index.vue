896fb5f6b39f3a52c26b5ee435ab4fca54b4711c
monoboza2 <thanathon.first2509@gmail.com>
2022-05-03 00:20:41 +0700

    matrix

--- pages/Matrix/Cramer/index.vue
+++ pages/Matrix/Cramer/index.vue
@@ -1,15 +1,38 @@
 <template>
   <div id="app">
     <h1>Finds</h1>
-    <div v-for="(find, index) in finds" :key="index">
-      <input v-model="find.value" :key="index" />
-    </div>
-    <button @click="addFind">New Find</button>
-    <button @click="print">Console</button>
     <div>
-      {{ finds }}
+      <p>MatrixA</p>
+      <input type="number" v-model.number="MatrixA.row" />
+      <input type="number" v-model.number="MatrixA.column" />
     </div>
+    <div v-for="n in MatrixA.row" :key="n + 'a'" style="display: flex">
+      <div v-for="i in MatrixA.column" :key="i + 'b'">
+        <input type="number" v-model.number="getfoo(n - 1)[i - 1]" />
+      </div>
+    </div>
+    <p>{{ MatrixA.Input }}</p>
+    <button @click="set0_A">Set0</button>
+    <div>
+      <p>MatrixB</p>
+      <input type="number" v-model.number="MatrixB.row" />
+      <input type="number" v-model.number="MatrixB.column" />
+    </div>
+    <div v-for="j in MatrixB.row" :key="j + 'c'" style="display: flex">
+      <div v-for="k in MatrixB.column" :key="k + 'd'">
+        <input type="number" v-model.number="getfoo2(j - 1)[k - 1]" />
+      </div>
+    </div>
+    <p>{{ MatrixB.Input }}</p>
+    <button @click="set0_B">Set0</button>
+    <!-- <div>
+      <input type="number" v-model.number="MatrixA.row" />
+      <input type="number" v-model.number="MatrixA.column" />
+    </div> -->
+    <!-- <p>{{ array }}</p> -->
   </div>
+  <!-- <p>{{ check() }}</p>
+    <p>{{ MatrixA.row }}</p> -->
 </template>
 
 <script>
@@ -17,20 +40,80 @@ export default {
   layout: 'core3',
   data() {
     return {
-      finds: [],
+      MatrixA: {
+        row: 0,
+        column: 0,
+        Input: [],
+      },
+      MatrixB: {
+        row: 1,
+        column: 0,
+        Input: [],
+      },
+      x: 2,
+      y: 2,
+      array: [],
     }
   },
   methods: {
-    addFind: function () {
-      this.finds.push({ value: '' })
-    },
-    print: function () {
-      let x = this.finds.length
-      for (let i = 0; i < x; i++) {
-        console.log(i + 1 + ':' + this.finds[i].value)
+    getfoo(n) {
+      if (!this.MatrixA.Input[n]) {
+        this.MatrixA.Input = []
+        for (let i = 0; i < this.MatrixA.row; i++) {
+          let row = []
+          for (let j = 0; j < this.MatrixA.column; j++) {
+            row.push(0)
+          }
+          this.MatrixA.Input.push(row)
+          console.log(this.MatrixA.Input)
+        }
       }
+      return this.MatrixA.Input[n]
+    },
+    getfoo2(n) {
+      if (!this.MatrixB.Input[n]) {
+        this.MatrixB.Input = []
+        for (let i = 0; i < this.MatrixB.row; i++) {
+          let row = []
+          for (let j = 0; j < this.MatrixB.column; j++) {
+            row.push(0)
+          }
+          this.MatrixB.Input.push(row)
+          console.log(this.MatrixB.Input)
+        }
+      }
+      return this.MatrixB.Input[n]
+    },
+    set0_A() {
+      return (this.MatrixA.Input = [])
+    },
+    set0_B() {
+      return (this.MatrixA.Input = [])
     },
   },
+  computed: {
+    // check: function () {
+    //   for (let i = 0; i < this.y; i++) {
+    //     let row = []
+    //     for (let j = 0; j < this.x; j++) {
+    //       row.push(0)
+    //     }
+    //     this.array.push(row)
+    //   }
+    //   console.log(this.array[0][0])
+    // },
+  },
+
+  // created: function () {
+  //   for (let i = 0; i < this.y; i++) {
+  //     let row = []
+  //     for (let j = 0; j < this.x; j++) {
+  //       row.push(0)
+  //     }
+  //     this.array.push(row)
+  //   }
+  //   console.log(this.array[0][0])
+  // },
 }
 </script>
 
