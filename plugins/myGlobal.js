d78b039801a24d586773f90efedfd999afb8f7da
monoboza2 <thanathon.first2509@gmail.com>
2022-05-26 12:40:31 +0700

    ci/cd

--- plugins/myGlobal.js
+++ plugins/myGlobal.js
@@ -1,4 +1,4 @@
 export default ({ $vuetify }, inject) => {
   // inject makes `$bp` available in context (the `$` prefix is automatically prefixed)
-  inject('bp','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InM2MjA0MDYyNjE2MDU3QGVtYWlsLmttdXRuYi5hYy50aCIsImlhdCI6MTY1MzUzNjIyMiwiZXhwIjoxNjUzNTM5ODIyLCJzdWIiOiIxIn0.kG8nvA7SSPgxdwHc4hdo0xSzyJC7jDJEB8Ra_Byzx-U' )
+  inject('bp','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InM2MjA0MDYyNjE2MDU3QGVtYWlsLmttdXRuYi5hYy50aCIsImlhdCI6MTY1MzU0MDYyNywiZXhwIjoxNjUzNTQ0MjI3LCJzdWIiOiIxIn0.UlGoTQ0KGyVh6MWin3ge7dHVc5IkZTwzYGvXjj7r7uA' )
 }
